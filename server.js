94f6bb27e2b148b270000bea4ec52c24b1f7ea51
kiw <106908925+Ikiw97@users.noreply.github.com>
2025-11-19 21:01:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,25 +13,19 @@ connectDB();
 
 const app = express();
 
-// Untuk __dirname pada ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Folder uploads diakses publik
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes backend
 app.use("/api", routes);
 
-// Error handler
 app.use(notFound);
 app.use(errorHandler);
 
-// Railway port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
