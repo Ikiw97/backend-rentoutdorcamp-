50a206ba9a18fa7d6abe673be7d8cc893ebabec8
kiw <106908925+Ikiw97@users.noreply.github.com>
2025-11-19 21:06:39 +0700

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,6 +3,9 @@ export const notFound = (req, res, next) => {
 };
 
 export const errorHandler = (err, req, res, next) => {
-  console.error("Error:", err.stack);
-  res.status(500).json({ message: err.message });
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
 };
