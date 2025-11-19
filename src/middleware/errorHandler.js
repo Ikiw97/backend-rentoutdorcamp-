af71054fbe0c0d0776433fa756f6b264767e07bc
kiw <106908925+Ikiw97@users.noreply.github.com>
2025-11-19 20:40:09 +0700

    Delete src directory

--- src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export const notFound = (req, res, next) => {
-  res.status(404).json({ message: `Route ${req.originalUrl} tidak ditemukan` });
-};
-
-export const errorHandler = (err, req, res, next) => {
-  console.error("Error:", err.stack);
-  res.status(500).json({ message: err.message });
-};
--- src/models/DamageReport.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const damageSchema = new mongoose.Schema({
-  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: "Booking" },
-  description: String,
-  cost: Number
-});
-
-export default mongoose.model("DamageReport", damageSchema);
