f0d20fbd1b33efdb5c5a7767153cb3d2940ea81b
kiw <106908925+Ikiw97@users.noreply.github.com>
2025-11-19 21:07:40 +0700

    Update Users.js

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,22 +3,15 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-
     email: { type: String, required: true, unique: true },
-
     password: { type: String, required: true },
-
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
     },
-
-    // Untuk upload KTP
     identityPhoto: { type: String, default: null },
-
     isVerified: { type: Boolean, default: false },
-
     isBanned: { type: Boolean, default: false },
   },
   { timestamps: true }
